ec9f98c3d9c6086577548d8657e0b8cdfe7ba864
Your Name <you@example.com>
2020-01-28 18:06:29 -0500

    filtered using open close operations

--- project1/hitandmiss.m
+++ project1/hitandmiss.m
@@ -10,12 +10,28 @@ img=rgb2gray(img);
 filtered_img=zeros(M,N);
 
 %applying median filter to remove salt and pepper noise
-for i=2:M-1
-    for j=2:N-1
-        filter=[img(i-1,j-1),img(i-1,j),img(i-1,j+1),img(i,j-1),img(i,j),img(i,j+1),img(i+1,j-1),img(i+1,j),img(i+1,j+1)];
-        filtered_img(i,j)=median(filter);
-    end
-end
+%for i=2:M-1
+ %   for j=2:N-1
+  %      filter=[img(i-1,j-1),img(i-1,j),img(i-1,j+1),img(i,j-1),img(i,j),img(i,j+1),img(i+1,j-1),img(i+1,j),img(i+1,j+1)];
+   %     filtered_img(i,j)=median(filter);
+    %end
+%end
+
+
+% Remove noise using open-close operations.
+strucElement1 = strel('disk',2); %strel function to create an structuring element
+img1 = imclose(img, strucElement1); %imclose to apply close operation
+strucElement2 = strel('disk',3)
+img2 = imopen(img1, strucElement2); %imclose to apply open operation
+% Primary image before noise removal
+figure
+imshow(img)
+% Image after removing noise
+figure
+imshow(img2)
+
 %converting the datatype of img from double to uint8
-filtered_img=uint8(filtered_img);
-imshow(filtered_img);
+%filtered_img=uint8(filtered_img);
+%imshow(filtered_img);
+
+
