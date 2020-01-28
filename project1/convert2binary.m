6acb9ec7f62d7162a4ca29071ceebeec885a84b4
Mudit Garg <anilmudit@gmail.com>
2020-01-28 18:51:58 -0500

    comments

--- project1/convert2binary.m
+++ project1/convert2binary.m
@@ -2,13 +2,16 @@ function y=convert2binary(img)
 
 [p,q,r]=size(img);
 
+%covnerting the colored image to grayscaled image
 if(r==3)
     img=rgb2gray(img);
 end
 
+%evaluating the threshold
 val=sum(sum(img));
-
 threshold=val/(p*q);
+
+%creating a binary image
 y=zeros(p,q);
 img=double(img);
 for i=1:p
@@ -18,5 +21,6 @@ for i=1:p
         end
     end
 end
+%changing the datatype of new image y
 y=uint8(y);
 return
