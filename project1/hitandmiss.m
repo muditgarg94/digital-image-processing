ee0881bbf39caa4befec8f68c01db820414fdb5b
Mudit Garg <anilmudit@gmail.com>
2020-01-30 18:00:52 -0500

    comments added

--- project1/hitandmiss.m
+++ project1/hitandmiss.m
@@ -3,17 +3,21 @@
 %      Perform the Hit-or-Miss Transformation on image "RandomDisks-P10"
 %
 % Input Variables:
-%      A     input the image "RandomDisks-P10.jpg"
-%      B     structuring element using which operation will be performed
+%      X     input binary image
+%      A     first structuring element
+%      B     second structuring element
 %      
 % Returned Results:
-%      noNoiseImg       finalImg
+%      transform       output image after hit and miss transformation
 %
 % Processing Flow:
 %      1.  
 %      2. 
 %
-%
+%   The following functions are called:
+%      erosion.m       perform the erosion operation
+%      complement.m    return the complement of image
+%      intersection.m  return the intersection of 2 images
 %
 %  Authors:      Mudit Garg, Mayank Murali, Niranjan Thirusaga
 %  Date:        1/30/2020
