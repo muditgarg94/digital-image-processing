34747c4e42747c77848e1a52304c5f7da7b547fc
Mudit Garg <anilmudit@gmail.com>
2020-03-23 01:05:08 -0400

    fixed diff

--- project3/anisotropic_diffusion.m
+++ project3/anisotropic_diffusion.m
@@ -22,43 +22,44 @@
 %  Date:        03/22/2020
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function diffused_img=anisotropic_diffusion(img, k)
+function diffused_img=anisotropic_diffusion(img, k,iter)
 
 [m,n]=size(img);
 lambda=0.25;
 
-%north gradient
-north=zeros(m,n);
-north(2:end,1:end)=img(1:end-1,1:end);
-north(1,:)=img(1,:);
-north_grad=north-img;
+for i=1:iter
+    %north gradient
+    north=zeros(m,n);
+    north(2:end,1:end)=img(1:end-1,1:end);
+    north(1,:)=img(1,:);
+    north_grad=north-img;
 
-%south gradient
-south=zeros(m,n);
-south(1:end-1,1:end)=img(2:end,1:end);
-south(end,:)=img(end,:);
+    %south gradient
+    south=zeros(m,n);
+    south(1:end-1,1:end)=img(2:end,1:end);
+    south(end,:)=img(end,:);
+    south_grad=south-img;
 
-south_grad=south-img;
+    %east gradient
+    east=zeros(m,n);
+    east(:,1:end-1)=img(:,2:end);
+    east(:,end)=img(:,end);
+    east_grad=east-img;
 
-%east gradient
-east=zeros(m,n);
-east(:,1:end-1)=img(:,2:end);
-east(:,end)=img(:,end);
+    %west gradient
+    west=zeros(m,n);
+    west(:,2:end)=img(:,1:end-1);
+    west(:,1)=img(:,1);
+    west_grad=west-img;
 
-east_grad=east-img;
+    %diffusion coefficients
+    cn=exp(-(north_grad./k).^2);
+    cs=exp(-(south_grad./k).^2);
+    ce=exp(-(east_grad./k).^2);
+    cw=exp(-(west_grad./k).^2);
 
-%west gradient
-west=zeros(m,n);
-west(:,2:end)=img(:,1:end-1);
-west(:,1)=img(:,1);
-west_grad=west-img;
+    img=img+lambda.*(cn.*north_grad+cs.*south_grad+ce.*east_grad+cw.*west_grad);
+end
 
-%diffusion coefficients
-cn=exp(-(north_grad./k).^2);
-cs=exp(-(south_grad./k).^2);
-ce=exp(-(east_grad./k).^2);
-cw=exp(-(west_grad./k).^2);
-
-diffused_img=img+lambda.*(cn.*north_grad+cs.*south_grad+ce.*east_grad+cw.*west_grad);
-
+diffused_img=img;
 return
