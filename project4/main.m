9c6d1d8070d23f5ec38e11935c40250daf1aee04
Your Name <you@example.com>
2020-04-10 14:47:07 -0400

    update

--- project4/main.m
+++ project4/main.m
@@ -1,4 +1,10 @@
+clc
+clear all
+clear
 
+% Converting GIF extension to PNG
+imageList = {'texture1.gif','texture2.gif', 'd4d29.gif','d9d77.gif'};
+giftopng(imageList)
 
 % Parameters initialization
 F = 0.059;
@@ -11,14 +17,17 @@ phi = 0;
 lambda = 1 ./ F;
 gamma = 1;
 
+
 % Reading the image
-%img = imread('texture2.gif');
-[I,cmap] = imread('texture1.gif','frames','all');
-image(I);
-colormap(cmap);
-[row, col, x] = size(I);
-
-% Apply Gabor Filter
-m1 = gaborFilter(I, gamma, lambda, theta, phi, sigma)
-
+imageList2 = {'texture1.png','texture2.png', 'd4d29.png','d9d77.png'};
+for i = 1 : numel(imageList2)
+  I = imread(imageList2{i});
+  %[row, col, x] = size(I);
+  imshow(I)
+ % Apply Gabor Filter
+  m1 = gaborFilter(I, gamma, lambda, theta, phi, sigma)
+  imshow(m1)
+  imwrite(m1,sprintf('%d.jpg',i))
 % Apply smoothing Filter
+% smoothImg = smoothingFilter();
+end
