a14adafceed62075646274033d7d44f5c3793791
Mudit Garg <anilmudit@gmail.com>
2020-04-22 03:21:47 -0400

    added commets

---
+++ project5/main.m
@@ -0,0 +1,18 @@
+%%%%%%%%%%%%% main.m file %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Purpose:
+%       To generate the fern shaped fractal
+%
+% Input Variables:
+%
+% Returned Results:
+%
+% Processing Flow:
+%
+% The following functions are called:  
+%       rederIFS.m
+%       
+%
+%  Author:      Mudit Garg, Mayank Murali, Niranjan Thirusangu
+%  Date:        04/22/2020
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
--- project5/renderIFS.m
+++ project5/renderIFS.m
@@ -1,30 +1,15 @@
 %%%%%%%%%%%%% renderIFS.m file %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Purpose:
-%       to apply the symmetric nearest neighbor mean on an image using a given mask
+%       To run the IFS algorithm given by Peitgin
 %
 % Input Variables:
-%       img     Input Image
-%       mask    Mask Size
 %
 % Returned Results:
-%       filtered_img      Image after applying snn mean filter on a given image
-%       img
 %
 % Processing Flow:
-%       1.  If the mask size is not given, default to 5.
-%       2.  Create a padding around the image.
-%       3.  Get the window equal to size of mask.
-%       4.  For each window, group together the symmetrical pairs.
-%       5.  Subtract the middle most element of the window with each value
-%       in pair.
-%       6. Select the pixel from each pair that is closed to middle pixel
-%       i.e have the least difference.
-%       7. Take the average of all these pixels.
-%       8. Again take the mean of the averaged value and the original pixel
-%       9. Set this mean value as the filtered image value.
 %
 % The following functions are called:  
-%       padding: Applies the padding of size mask/2 to the given image and returns the padded image. 
+%       win.m
 %
 %  Author:      Mudit Garg, Mayank Murali, Niranjan Thirusangu
 %  Date:        04/22/2020
